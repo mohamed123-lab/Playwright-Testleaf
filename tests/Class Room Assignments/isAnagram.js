b9607294cf1b3f1dc06a65f586e3194ce2b93e98
Mohamed Rafic <mohamedrafic@gmail.com>
2026-07-25 23:44:15 -0400

    update

--- tests/Class Room Assignments/isAnagram.js
+++ tests/Class Room Assignments/isAnagram.js
@@ -1,8 +1,8 @@
 function isAnagram(string1, string2) {
     // Convert to lowercase, remove spaces, split into characters,
     // sort them, and join back into strings
-    string1 = string1.toLowerCase().split("").sort().join("");
-    string2 = string2.toLowerCase().split("").sort().join("");
+    string1 = string1.toUpperCase().split("").sort().join("");
+    string2 = string2.toUpperCase().split("").sort().join("");
 
    return string1 === string2;
 }
@@ -16,6 +16,7 @@ let testCases = [
 
 for (let i = 0; i < testCases.length; i++) {
     let [string1, string2] = testCases[i];
+    console.log(testCases[i])
 
     if (isAnagram(string1, string2)) {
         console.log(`${string1} and ${string2} are anagrams.`);
