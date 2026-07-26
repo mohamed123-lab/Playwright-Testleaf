68c1956a166a7f324b859c8ed3dc541d3c6ab664
Mohamed Rafic <mohamedrafic@gmail.com>
2026-07-26 09:08:40 -0400

    Added Day01-Day05 from trainer

--- tests/Day01/firstProgram.js
+++ tests/Day01/firstProgram.js
@@ -1,4 +1,4 @@
-console.log('Mohamed')
+console.log('bhuvanesh')
 console.log('Hello World')
 // command to execute the file from the terminal : node fileName.js
 // (''),(""),(``) are used to define strings in JavaScript.
